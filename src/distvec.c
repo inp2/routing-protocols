5b1c2c2d003127b944af86cbc7a4ac66669ac307
inp2 <ipalmer2@illinois.edu>
2015-11-04 13:31:13 -0600

    split the topofile

--- src/distvec.c
+++ src/distvec.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
+void read_file(std::string filename)
+{
+	std::string line;
+	std::ifstream topo (filename);
+	if(topo.is_open())
+	{
+		while(getline(topo,line))
+		{
+			std::istringstream iss(line);
+
+			do
+			{
+				std::string sub;
+				iss >> sub;
+				std::cout << sub << std::endl;
+			}while(iss);
+		}
+		topo.close();
+	}
+	else
+	{
+		std::cout << "Unable to open file";
+	}
+}
 int main(int argc, char *argv[])
 {
 	if (argc != 4) {
@@ -13,5 +39,5 @@ int main(int argc, char *argv[])
 	std::string topofile  = argv[1];
 
 	// Read topofile, and parse topofile	
-	
+	read_file(topofile);	
 }
